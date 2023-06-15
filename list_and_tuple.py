118361c42ad696a9a2a2dde8230eea9620c1c274
Manoj <manojpatil9147@gmail.com>
2023-06-15 15:11:25 +0530

    Initial commit

---
+++ list_and_tuple.py
@@ -0,0 +1,26 @@
+# li= [2,4,6,6,4,'W']
+# print(li)
+# li[5]=2
+# li.append(2)
+# print(li)
+# # print("total score: ",sum(li))
+# total=0
+# for a in li:
+#     total+=a
+# print("total : ",total)
+
+
+# li=[12,13,14,15,1,15,154,646,64]
+# # print(min(li))
+# a=li[0]
+# index=0
+# for i in range(len(li)):
+#     if a>li[i]:
+#         index=i
+#         a=li[i]
+# print(a,index)
+
+#develop a python program to convert a list of characters into string
+li=['a', 'b', 'h', 'i', ' ', 'i', 's', ' ', 'a', ' ', 'g', 'o', 'o', 'd', ' ', 'b', 'o', 'y']
+print("".join(li))
+hkjhfkjhd
