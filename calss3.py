821b527bdbb61fd95100212c44b0de67f623a946
Manoj <manojpatil9147@gmail.com>
2023-06-15 14:39:13 +0530

    first commit

---
+++ calss3.py
@@ -0,0 +1,53 @@
+#5 june 2023
+
+# #while runs until we enter y or Y
+# a=input("enter your choice")
+# while a.lower()!='y':
+#     print("running")
+#     a = input("enter your choice")
+# print("bye")
+
+# #run while loop until counter reaches 5
+# counter = 0
+# while counter<5:
+#     print(counter,end=" ")
+#     counter+=1
+# print("\nthe loop has end")
+
+# while True:
+#     a= input("enter a number or exit to end ")
+#     if a =="exit":
+#         break
+#     a=int(a)
+#     print(a," square is ",a**2)
+# print("done")
+# sum=0
+# for i in range(5):
+#     sum+=i
+# print("sum= ",sum)
+
+
+#test score finder
+# print("The Test Score program\n")
+# total=0
+# count=0
+# print("Enter 999 to end input")
+# print("=====================================================")
+# score=int(input("Enter the test score: "))
+# while score!=999:
+#     if score>100 or score<0:
+#         print("Test score must be between 0-100")
+#     else:
+#         total+=score
+#         count+=1
+#     score = int(input("Enter the test score: "))
+# avg= total/count if count!=0 else 0
+# print("=====================================================")
+# print("total score: ",total)
+# print("Average score: ",avg)
+#
+# def thapa(a=1):
+#     for _ in range(a):
+#         print("java jayesh")
+#
+# thapa()
---
+++ class1.py
@@ -0,0 +1,34 @@
+# # date: 30 May 2023
+#
+# # Multiline
+# print("hello"
+#       + " there!")
+# print("hello" + " hey" +\
+#       " there!!")
+#
+# a=12
+# b=13
+# print("a+b=",a+b)
+# print("a-b=",a-b)
+# print("a/b=",a/b)
+# print("a//b=",a//b)
+# print("a%b=",a%b)
+# print(3+5*4,(3+5)*4)
+#
+# #largest of 2
+# print("larger=",max(input("enter 2 numbers:").split()))
+#
+# #area of circle
+# print("Area=",3.14*int(input("enter radius="))**2)
+
+#working with string
+s="Manoj"
+s+=" Patil"
+print(s)
+
+a={1,2,3}
+a.discard(0)
+print(a)
+
+
+
