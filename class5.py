6055a11c496eb0d2f4d5e522156c687eb07505f5
Manoj <manojpatil9147@gmail.com>
2023-06-20 13:31:55 +0530

    added class work of 10 june 2023

---
+++ class5.py
@@ -0,0 +1,25 @@
+#20 june 2023
+
+#list inside a list
+
+# movies=[["The Holy Grail",1975,9.99],
+#         ["Life if Brain",1979,12.30],
+#         ["The Meaning if Life",1997,28.90]]
+# print(movies)
+
+# try this out
+# for a in movies:
+#     print(*a)
+
+# li=[12,14,15,14,16]
+# print(li.count(14))
+# li.reverse()
+# print(li)
+# li.sort()
+# print(li)
+
+
+seq=[12,14,15,15,16,18,18,10]
+print(seq[1:10])
+
+
