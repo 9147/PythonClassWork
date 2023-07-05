2346da4ddeb0e2a960862a6b1ebe355121082ee9
Manoj <manojpatil9147@gmail.com>
2023-07-05 22:13:33 +0530

    added class work of 05 july 2023

---
+++ class7.py
@@ -0,0 +1,22 @@
+#05 july 2023
+
+#string handling
+# s="this is me!!"
+# print(s.startswith("thi"))
+# print(s.title())
+#
+# message= "   this string is the concept we are studying   "
+# print("lower case: ",message.lower())
+# print("Upper case: ",message.upper())
+# print("Starts with : ",message.startswith('a'))
+# print("title case: ",message.title())
+# print("Removed spaces: ",message.strip())
+# print("Ends with studying: ",message.endswith("studying"))
+
+# s= "megha pagal hai!!"
+# s=s.replace('megha',"maggie")
+# print(s)
+
+# s="this is me!! "
+# print(s.find('me'))
+
