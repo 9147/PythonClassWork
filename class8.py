dfaa6fea5bf961c519736960ee9cd51789b1a823
Manoj <manojpatil9147@gmail.com>
2023-07-17 13:08:05 +0530

    added class work of 17 july 2023

---
+++ class8.py
@@ -0,0 +1,27 @@
+'''
+
+  about files
+'''
+import csv
+
+#
+# members = []
+# with open('message.txt') as file:
+#     for line in file:
+#         line = line.replace('\n','')
+#         members.append(line)
+#
+# print(members)
+
+
+# movies=[['Monty Python ans the HOly Grail',1975],
+#         ['Cat on a Hot Tin Roof',1951],
+#         ['On the Waterfront',1954]]
+# with open("movies.csv",'w',newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(movies)
+
+# with open("movies.csv",newline="") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row[0]+ "(" +str(row[1]) +")")
